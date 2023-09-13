b93c4197e38fe481b2b765da2b7378dfd579d7d4
Leila.bwt <leila.bwt@gmail.com>
2023-09-13 20:40:10 -0300

    join e paginação de resultados

--- 15-ConexaoNodePostgre/index.js
+++ 15-ConexaoNodePostgre/index.js
@@ -5,20 +5,49 @@ const pool = require('./conexao');
 
   app.use(express.json());
 
-  app.get('/:id', async (req, res) => {
-    const{id} = req.params
-  try {
-    const resultado = await pool.query(
-      `SELECT * from empresas where id = ${id}`
-      ); // where id passado no params
-      // DE FORMA ALGUMA DEVEMOS CONCATENAR REGISTROS DENTRO DE UMA QUERY
-      // POIS ISSO PODE GERAR SQL INJECTION QUE É UMA FORMA DE ATAQUE
-      // O RISCO É ALTO, POIS O HACKER PODE APAGAR TODOS OS REGISTROS
-    return res.json(resultado.rows[0]);
+  app.get('/', async (req, res) => {
+    const{pagina, porPagina} = req.query
+   try {
+    //const query = 'SELECT * from empresas where nome = $1 or nome = $2';  
+    //const params = ['Google', 'Facebook']
+
+    //const query = 'update empresas set site = $1 where id = $2';
+    //const params = ['www.cakewalk.com', 1]
+
+    /*const query = `
+    select e.id as empresaId, f.id as filialId, e.nome, f.pais, p.nome as funcionario
+    from empresas e
+    join filiais f on e.id = f.empresa_id
+    join pessoas p on e.id = p.empresa_id
+    `;*/
+
+    /*const query = `
+    select e.id as empresaid, f.id as filialid, e.nome, f.pais 
+    from empresas e right join filiais f on e.id = f.empresa_id;
+    `;*/
+
+    /*const query = `
+    select e.id as empresaid, f.id as filialid, e.nome, f.pais
+     from empresas e full join filiais f on e.id = f.empresa_id;`*/
+
+    const query = ` select * from pessoas order by id asc limit $1 offset $2;`
+
+    const {rowCount} = await pool.query(`select * from pessoas`);
+    const offset = pagina === 1 ? 0 : (pagina - 1) * porPagina;
+
+    const resultado = await pool.query(query, [porPagina,offset]);
+    const result = {
+      pagina,
+      porPagina,
+      total: rowCount,
+      registros: resultado.rows
+    }
+    return res.json(result);
   } catch (error) {
     console.log(error.message);
   }
 
+ 
 });
 
 app.listen(3000);
