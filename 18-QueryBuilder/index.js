b683ec54da695bb165e5a115e277c003e5f08cb6
Leila.bwt <leila.bwt@gmail.com>
2023-09-26 17:05:51 -0300

    first

--- 18-QueryBuilder/index.js
+++ 18-QueryBuilder/index.js
@@ -10,7 +10,14 @@ app.get('/', async (req, res) => {
     //const agenda = await knex('agenda').debug();//debug mostra o sql que foi executado
     //const agenda = await knex.raw('select * from agenda');
     //const agenda = await knex('agenda').where('id', '!=', 5).debug();
-    const agenda = await knex('agenda').where({id:5}).debug();
+    //const agenda = await knex('agenda').where({id:5}).debug();
+
+    //const agenda = await knex('agenda').where({id:5}).first().debug();
+    //first() retorna apenas o primeiro registro
+
+    const agenda = await knex('agenda').where({id:5}).select('id', 'nome').first().debug();
+
+    
     return res.json(agenda);
 });
 
